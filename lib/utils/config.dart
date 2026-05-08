3ad0627e47d26b44126e5be1b760ac6c8b2c21f2
Mhezzy155 <meisyaandaryani600@gmail.com>
2026-05-08 16:22:30 +0800

    add config component

---
+++ lib/utils/config.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+
+  // width height init
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData!.size.width;
+    screenWidth = mediaQueryData!.size.height;
+  }
+
+  // spacing height
+  static const smallSpacing = SizedBox(height: 25);
+  static final mediumSpacing = SizedBox(height: screenHeight! * 0.5);
+  static final largeSpacing = SizedBox(height: screenHeight! * 0.8);
+
+  // textform field border
+  static const outlinedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  );
+
+  static const focusBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(color: Colors.greenAccent),
+  );
+
+  static const errorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(color: Colors.red),
+  );
+}
