a6b524f993d6f9af9814463907989105a76c75bd
Mhezzy155 <meisyaandaryani600@gmail.com>
2026-05-08 16:35:30 +0800

    add pre-define text

---
+++ lib/utils/text.dart
@@ -0,0 +1,13 @@
+// pre-define text
+
+class AppText {
+  static final enText = {
+    'welcome_text':'Welcome',
+    'signIn_text': 'Sign In to your account',
+    'registered_text':'Already have an Account?',
+    'register_text':'You can easily Sign Up and connect to the Doctors nearby',
+    'signUp_text':'Don\'t have any account?',
+    'social-login': 'Or continue with your social account?',
+    'forgot-password': 'Forgot your password?',
+  };
+}
