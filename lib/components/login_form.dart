2cba5b84b4a8622ba00f8d794541f04ea08d32fb
Mhezzy155 <meisyaandaryani600@gmail.com>
2026-05-12 20:46:37 +0800

    add login form

---
+++ lib/components/login_form.dart
@@ -0,0 +1,81 @@
+import 'package:doctor_app/components/button.dart';
+import 'package:doctor_app/utils/config.dart';
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  bool obsecurePass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Config.primaryColor,
+            decoration: InputDecoration(
+              hintText: 'Email Address',
+              labelText: 'Email',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Config.primaryColor,
+            ),
+          ),
+          Config.smallSpacing,
+          TextFormField(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Config.primaryColor,
+            obscureText: obsecurePass,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.lock_outline),
+              prefixIconColor: Config.primaryColor,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    obsecurePass = !obsecurePass;
+                  });
+                },
+                icon: obsecurePass
+                    ? 
+                    Icon(
+                      Icons.visibility_off_outlined, 
+                      color: Colors.black
+                    )
+                    : Icon(
+                        Icons.visibility_outlined,
+                        color: Config.primaryColor,
+                      ),
+              ),
+            ),
+          ),
+          Config.smallSpacing,
+          Button(
+            width: double.infinity, 
+            title: 'Sign In', 
+            disable: false, 
+            onPressed: (){},
+            padding: EdgeInsets.all(15),
+          ),
+        ],
+      ),
+    );
+  }
+}
